9f13e96d77a6d05547d45424ba6e57bc6b810935
Unwana Ekpo <brettygeorge3@gmail.com>
2022-09-02 02:55:57 +0100

    added the wallet table to users

---
+++ routes/api.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::post('/users/wallet', [UserController::class, 'addMoney']);
+Route::post('/users/wallet/{id}', [UserController::class, 'removeMoney']);
